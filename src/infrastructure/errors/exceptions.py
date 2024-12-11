e7a5a7c0643edc099114da926cbc6cd86acf1601
Dmytro Parfeniuk <parfeniukink@gmail.com>
2024-12-12 00:32:36 +0200

    ✨ `UnprocessableRequestError` is introduced

--- src/infrastructure/errors/exceptions.py
+++ src/infrastructure/errors/exceptions.py
@@ -27,6 +27,33 @@ class BadRequestError(BaseError):
         )
 
 
+class UnprocessableRequestError(BaseError):
+    """consider cases when the server can not perform the operation due
+    wrong request from the user.
+
+    notes:
+        if `BadRequestError(400)` is raised in case user input data can't be
+            validated or something else, the `UnprocessableRequestError(422)`
+            is raised only if the input data is properly set, but system CAN
+            NOT change its state because of any reasons. let's say you'd like
+            to user some sort of INSERT command but due to the duplication in
+            the database the operation can't be performed.
+
+            using another explaination: if HTTP POST /resource returns 400, it
+            is going to return 400 Bad Request all the time. Otherwise, if
+            HTTP POST /resource returns 200 OK, it may return 422 Unprocessable
+            Entity for the second request and again 200 OK for the 3-rd request
+            if the business logic doesn't mind.
+
+    """
+
+    def __init__(self, message="Unprocessable entity") -> None:
+        super().__init__(
+            message=message,
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        )
+
+
 class NotFoundError(BaseError):
     """consider cases when the resourse can't be found for this operation."""
 
